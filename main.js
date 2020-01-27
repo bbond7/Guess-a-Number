08df341861dbe3e92a990ffc15c231827ef566b2
Bryan Bond <bbond@student.neumont.edu>
2020-01-26 22:59:19 -0700

    Finished up Java project

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-var randNum = Math.round(Math.random()*99 ) + 1;
+var randNum = Math.round((Math.random() * 100) + 1);
+var counter= 0;
+
 
 function sendData(form) {
     var guessNum = form.inputbox.value;
+    form.inputbox.value = "";
+    counter += 1;
 
-if (randNum === guessNum) {
-    document.getElementById("answer").innerText = "Congrats you Won!!"
+if (randNum == guessNum) {
+    document.getElementById("answer").innerText = "Congrats you won!! It took you " + counter + " guesses." 
 } else if (randNum > guessNum) {
     document.getElementById("answer").innerText = "Too Low, try again"
 } else if (randNum < guessNum) {
-    document.getElementById('answer').innerText = "Too high, try again"
-} 
-
+    document.getElementById("answer").innerText = "Too high, try again"
+} else if (isNaN(guessNum) || guessNum < 1 || guessNum > 100) {
+    document.getElementById("answer").innerText = "Not Valid, try again"
 }
+}
+
+console.log(randNum);
