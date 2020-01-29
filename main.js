e0917861ed8ac8524b649e997b610f36cb12fc24
Bryan Bond <bbond@student.neumont.edu>
2020-01-28 18:13:37 -0700

    added a Gif for winning

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var randNum = Math.round((Math.random() * 100) + 1);
 var counter= 0;
 
 
+
 function sendData(form) {
     var guessNum = form.inputbox.value;
     form.inputbox.value = "";
     counter += 1;
 
 if (randNum == guessNum) {
-    document.getElementById("answer").innerText = "Congrats you won!! It took you " + counter + " guesses."
+    document.getElementById("answer").innerText = "Congrats you won!! It took you " + counter + " guesses.";
+    document.getElementById("winner").setAttribute("src", "https://media1.tenor.com/images/5ebdda822f3e1f9cf35130443f24de80/tenor.gif?itemid=9151673")
 } else if (randNum > guessNum) {
     document.getElementById("answer").innerText = "Too Low, try again"
 } else if (randNum < guessNum) {
